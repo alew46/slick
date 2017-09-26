c9ad877867fe688f74528411e3812acf9657012d
Alex Lewis <alew46@gmail.com>
2017-09-26 16:03:07 -0400

    add messages working

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -1,18 +1,36 @@
 (function() {
-    function RoomCtrl(Room, Messages) {
+    function RoomCtrl(Room, Messages, $cookies) {
         this.allRooms = Room.getAll();
         
         this.setActiveRoom = function(room) {
             this.activeRoom = room;
-            this.activeRoom.name = room.$value;
+            this.activeRoom.name = room.$id;
             console.log("Active room is now " + this.activeRoom.name);
             // we need to pull in messages by room id
             this.messages = Messages.getByRoomId(room.$id);
+        };
+        
+        this.newMessageText = '';
+        
+        this.sendMessage = function () {
+            Messages.send({
+                content: this.newMessageText,
+                roomId: this.activeRoom.name,
+                username: $cookies.get('blocChatCurrentUser')
+            });
+            
+            this.newMessageText = '';
+        };
+        
+        this.checkKey = function (event) {
+            if (event.which === 13 || event.keyCode === 13) {
+                this.sendMessage();
+            }
         };
         
     }
             
     angular
         .module('slick')
-        .controller('RoomCtrl', ['Room', 'Messages', RoomCtrl]);
+        .controller('RoomCtrl', ['Room', 'Messages', '$cookies', RoomCtrl]);
 })();
--- app/scripts/services/Messages.js
+++ app/scripts/services/Messages.js
@@ -1,6 +1,7 @@
 (function() {
-    function Messages($firebaseArray) {
-        var Messages = {}
+    function Messages($firebaseArray, Room) {
+        var Messages = {};
+        
         var ref  = firebase.database().ref().child("messages");
         
         var messages = $firebaseArray(ref);
@@ -8,12 +9,17 @@
         Messages.getByRoomId = function(roomId) {
             return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
         };
+        
+        Messages.send = function(newMessage) {
+            console.log("test inside send");
+            messages.$add(newMessage);
+        };
                 
         return Messages;
     }
     
     angular
         .module('slick')
-        .factory('Messages', ['$firebaseArray', Messages]);
+        .factory('Messages', ['$firebaseArray', 'Room', Messages]);
     
 })();
